acd78dc9b18b04fef5bfe77f815058bc1096f66a
Estela Medrano <estela.medrano@creditas.com>
2020-03-23 09:22:58 +0100

    generated service layer

--- src/main/kotlin/com/estela/product/ProductApplication.kt
+++ src/main/kotlin/com/estela/product/ProductApplication.kt
@@ -2,6 +2,7 @@ package com.estela.product
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,9 +15,28 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-@RequestMapping("")
-class helloController{
-
+@RequestMapping("/api/v1/product")
+class productController(
+		private val productService: ProductService
+){
 	@GetMapping
-	fun hello() = "Hello Worlded"
+	fun findAll() = productService.findAll()
+}
+
+@Service
+class ProductService{
+	private val products:Set<Product> = setOf(Product("Apple", 22.2), Product("Banana", 22.3))
+
+	fun findAll():List<Product> = products.toList()
+}
+
+data class Product(val name:String,val price:Double?=22.3){
+	override fun equals(other: Any?): Boolean {
+		other ?: return false
+		if(other === this)return true
+		if(this.javaClass != other.javaClass) return false
+		other as Product
+
+		return this.name == other.name
+	}
 }
