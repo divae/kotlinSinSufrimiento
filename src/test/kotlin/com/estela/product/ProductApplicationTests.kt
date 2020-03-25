7323e0fb51517dfb19efb7d4b8f546cebccfdd9b
Estela Medrano <estela.medrano@creditas.com>
2020-03-25 23:05:32 +0100

    added integration test to get product

--- src/test/kotlin/com/estela/product/ProductApplicationTests.kt
+++ src/test/kotlin/com/estela/product/ProductApplicationTests.kt
@@ -1,16 +1,52 @@
 package com.estela.product
 
+import com.estela.product.domain.Product
+import com.estela.product.service.ProductService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class ProductApplicationTests {
 
+	@Autowired
+	private lateinit var mockMvc: MockMvc
+
+	@Autowired
+	private lateinit var mapper: ObjectMapper
+
+	@Autowired
+	private lateinit var productService: ProductService
+
 	@Test
-	fun contextLoads() {
+	fun `Get() - returns the list of products if was successful`() {
+		val productsFromService = productService.findAll()
+		val json = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
+				.andExpect(status().isOk)
+				.andReturn().response.contentAsString
+
+		val products:List<Product> = mapper.readValue(json)
+
+		assertThat(productsFromService, Matchers.`is`(Matchers.equalTo(products)))
+	}
+
+	@Test
+	fun `Get() - if not exist returns not found`() {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(status().isNotFound)
+				.andReturn().response.contentAsString
 	}
 
 }
